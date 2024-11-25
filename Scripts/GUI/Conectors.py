be19216e34a4e0506d60d1fbbe4dbced3cab6513
Orlanstein <o.g.j.c051204@gmail.com>
2024-11-24 22:51:38 -0600

    Added functions to convert from yt or local video

--- Scripts/GUI/Conectors.py
+++ Scripts/GUI/Conectors.py
@@ -8,18 +8,33 @@ from VideoProcessing.VIdeoDownloader import descargar_video
 from VideoProcessing.VideoToBinaryVideo import convert_video_to_binary_bw
 from VideoProcessing.BinVidToCodeTxt import video_to_oled_data
 
-# Descarga el video
-descargar_video("https://www.youtube.com/watch?v=rwKhWgYKXME")
+def ConvertFromYouTube(linkYTVideo):
+    # Descarga el video
+    descargar_video(linkYTVideo)
 
-# Rutas de entrada y salida
-ruta_video_descargado = os.getcwd() + "/Video/VideoDownloaded/DownloadedVideo.mp4"
-# Añadimos el nombre del archivo de salida a la ruta
-ruta_videoReescalado_BW = os.getcwd() + "/Video/VideoReescaled/video_bw_128x64.avi"  # <- Añadido nombre del archivo
-# Ruta salida del txt
-ruta_txt_salida = os.getcwd() + "/Output/arrayVideoOLED.txt"
+    # Rutas de entrada y salida
+    ruta_video_descargado = os.getcwd() + "/Video/VideoDownloaded/DownloadedVideo.mp4"
+    # Añadimos el nombre del archivo de salida a la ruta
+    ruta_videoReescalado_BW = os.getcwd() + "/Video/VideoReescaled/video_bw_128x64.avi"  # <- Añadido nombre del archivo
+    # Ruta salida del txt
+    ruta_txt_salida = os.getcwd() + "/Output/arrayVideoOLED.txt"
 
-# Reescala y convierte a blanco y negro
-convert_video_to_binary_bw(ruta_video_descargado, ruta_videoReescalado_BW)
+    # Reescala y convierte a blanco y negro
+    convert_video_to_binary_bw(ruta_video_descargado, ruta_videoReescalado_BW)
 
-#Covierte video en BW y reescalado a un txt
-video_to_oled_data(ruta_videoReescalado_BW, ruta_txt_salida)
+    #Covierte video en BW y reescalado a un txt
+    video_to_oled_data(ruta_videoReescalado_BW, ruta_txt_salida)    
+
+def ConvertFromLocalVideo(pathLocalVideo):
+    # Rutas de entrada y salida
+    ruta_video_descargado = pathLocalVideo
+    # Añadimos el nombre del archivo de salida a la ruta
+    ruta_videoReescalado_BW = os.getcwd() + "/Video/VideoReescaled/video_bw_128x64.avi"  # <- Añadido nombre del archivo
+    # Ruta salida del txt
+    ruta_txt_salida = os.getcwd() + "/Output/arrayVideoOLED.txt"
+
+    # Reescala y convierte a blanco y negro
+    convert_video_to_binary_bw(ruta_video_descargado, ruta_videoReescalado_BW)
+
+    #Covierte video en BW y reescalado a un txt
+    video_to_oled_data(ruta_videoReescalado_BW, ruta_txt_salida)  
